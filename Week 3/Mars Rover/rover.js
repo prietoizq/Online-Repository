b57bf9225dcf764c02ca6d5dac83de9d856127d3
Angel <prietoizq@gmail.com>
2015-04-23 17:56:28 +0200

    Rover primer intento

---
+++ Week 3/Mars Rover/rover.js
@@ -0,0 +1,64 @@
+var variable = {position: [0,0], direction: 'N'};
+
+function goForward(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--;
+      break;
+    case 'E':
+      rover.position[1]++;
+      break;
+    case 'S':
+      rover.position[0]++;
+      break;
+    case 'W':
+      rover.position[1]--;
+      break;
+  }
+}
+
+function goBack(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]++;
+      break;
+    case 'E':
+      rover.position[1]--;
+      break;
+    case 'S':
+      rover.position[0]--;
+      break;
+    case 'W':
+      rover.position[1]++;
+      break;
+  }
+}
+
+function turnLeft(rover){
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'E':
+      rover.direction = 'N';
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+  }
+}
+
+function turnRight
+
+
+//goForward(myRover);
+
+var grid = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
+
+grid[7][3]="X";
+console.log(grid);
+
+
