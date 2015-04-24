16955d118d32a102e1dea8576036cbad260698bd
Angel <prietoizq@gmail.com>
2015-04-24 16:40:40 +0200

    Segunda iteracion Mars Rover

--- Week 3/Mars Rover/rover.js
+++ Week 3/Mars Rover/rover.js
@@ -74,20 +74,23 @@ function turnRight(rover){
 }
 
 function updateRover(rover){
-   
+  
+    var y = rover.position[0]; 
+    var x = rover.position[1];
+    if(y=== -1){rover.position[0] = 9}; //con estas condiciones hacemos que si se sale de la cuadrícula, aparezca en el otro lado
+    if(y=== 10){rover.position[0] = 0};
+    if(x=== -1){rover.position[1] = 9};
+    if(x=== 10){rover.position[1] = 0};
 
- //   var y = rover.positionOld[0];
- //   var x = rover.positionOld[1];
+    grid[rover.positionOld[0]][rover.positionOld[1]] = 0; //el valor que ocupaba el rover (R) en la posición anterior se sustituye con un 0
     
+    console.log(rover); //Antes de actualizar la posición antigua con la nueva, se muestra el rover en la consola para que puedan verse las diferencias entre las dos posiciones
 
-    grid[rover.positionOld[0]][rover.positionOld[1]] = 0;
     
-    console.log(rover);
-    
-    grid[rover.position[0]][rover.position[1]] = "R";
+    grid[rover.position[0]][rover.position[1]] = "R"; //el nuevo valor que ocupa el rover (R)
     console.log(grid);
     
-    rover.positionOld[0] = rover.position[0];
+    rover.positionOld[0] = rover.position[0]; //se actualiza la posición antigua con la nueva
     rover.positionOld[1] = rover.position[1];
 
 }
@@ -95,6 +98,25 @@ function updateRover(rover){
 function seeRover(rover){
   console.log(grid);
   console.log(rover);
+}
+
+function control(rover, orders){
+    for(var i=0, l=orders.length; i<l; i+=1){
+      switch (orders[i]){
+        case 'f':
+          goForward(rover);
+          break;
+        case 'b':
+          goBack(rover);
+          break;
+        case 'r':
+          turnRight(rover);
+          break;
+        case 'l':
+          turnLeft(rover);
+          break;
+      }
+    }
 }
 
 
